0325d0be150e202d24377e682a850635d550b0ce
Remsey Mailjard <remsey.mailjard@gmail.com>
2023-12-07 23:13:33 +0100

    teste

---
+++ test.js
@@ -0,0 +1,8 @@
+Client ID
+204562417527-d4f58tijlbsu1pmm2qim6prdmp6r7p2k.apps.googleusercontent.com
+Client secret
+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
+Creation date
+December 7, 2023 at 10:47:12 PM GMT+1
+Status
+ Enabled
